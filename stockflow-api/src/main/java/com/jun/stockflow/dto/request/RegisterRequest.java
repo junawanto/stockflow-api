39c1320354c128bee7b83925687899c612a26d3c
Jun <junawanto.candra@gmail.com>
2026-07-06 08:29:05 +0700

    Add authentication DTOs

---
+++ stockflow-api/src/main/java/com/jun/stockflow/dto/request/RegisterRequest.java
@@ -0,0 +1,30 @@
+package com.jun.stockflow.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+
+    @NotBlank
+    private String fullName;
+
+    @NotBlank
+    @Size(min = 4, max = 50)
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+
+    @NotBlank
+    private String roleName;
+}
